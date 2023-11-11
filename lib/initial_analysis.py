4b0e50bdf196c2120d8f5d0be71a82c6af94ef88
ByronHernandez <bhernandezosorio@ufl.edu>
2023-11-11 08:30:37 -0500

    initial analysis almost complete

--- lib/initial_analysis.py
+++ lib/initial_analysis.py
@@ -2,29 +2,44 @@
 Initial script to load data from the database
 '''
 
-import os
 from .seera import SEERA
 
-def initial_analysis():
-    os.system('clear')
-    seera = SEERA('data/seera.xlsx')
+def analyze_significance(seera):
     seera.print_column_names()
     seera.print_groups_indexes()
     seera.print_group_names()
+
+def analize_attributes(seera):
+    complete = 0
+    print('\n\nAttribute' + ' ' * 42 + " 'nan'     'miss'     'ok'       'total'\n" + "-" * 96)
+    for key in seera.names:
+        name, analysis = seera.analyze_missing_values_per_attribute(key)
+        if analysis['ok'] == analysis['total']: complete += 1
+        print(name + ' ' * (50 - len(name)), analysis)
+    print('Number of attributes in all the projects:', complete)
+
+def analize_projects(seera):
+    complete = 0
+    print("\n\nPrj  'nan'     'miss'     'ok'      'total'\n" + "-" * 47)
+    for i in range(len(seera.data)):
+        name, analysis = seera.analyze_missing_values_per_project(i)
+        if analysis['ok'] == analysis['total']: complete += 1
+        print(name, analysis)
+    print('Number of projects with all the attributes:', complete)
+
+
+def initial_analysis():
+    seera = SEERA('data/seera.xlsx')
+    
+    analyze_significance(seera)
+
     name, analysis = seera.analyze_missing_values_per_attribute('% project gain (loss)')
     print(name, analysis, '\n')
     name, analysis = seera.analyze_missing_values_per_attribute(12)
     print(name, analysis)
 
-    print('\n\nAttribute' + ' ' * 42 + " 'nan'     'miss'     'ok'       'total'\n" + "-" * 96)
-    for key in seera.names:
-        name, analysis = seera.analyze_missing_values_per_attribute(key)
-        print(name + ' ' * (50 - len(name)), analysis)
-
-    print("\n\nPrj  'nan'     'miss'     'ok'      'total'\n" + "-" * 47)
-    for i in range(len(seera.data)):
-        name, analysis = seera.analyze_missing_values_per_project(i)
-        print(name, analysis)
+    analize_attributes(seera)
+    analize_projects(seera)
 
     columns = [
                'ProjID', 
@@ -56,22 +71,14 @@ def initial_analysis():
     for column in out_columns:
         seera.delete_column(column)
 
-    # seera.add_column('Actual zeros', 'General Information', [0] * len(seera.data)) # Works
+    print('\n' + '=' * 28 + '\n=> After deleting columns <=\n' + '=' * 28 + '\n')
+    
+    analyze_significance(seera)
+    analize_attributes(seera)
+    analize_projects(seera)
 
-    print('\n\n')
-    seera.print_column_names()
-    seera.print_groups_indexes()
-    seera.print_group_names()
-
-    print('\n\nAttribute' + ' ' * 42 + " 'nan'     'miss'     'ok'       'total'\n" + "-" * 96)
-    for key in seera.names:
-        name, analysis = seera.analyze_missing_values_per_attribute(key)
-        print(name + ' ' * (50 - len(name)), analysis)
-
-    complete = 0
-    print("\n\nPrj  'nan'     'miss'     'ok'      'total'\n" + "-" * 47)
-    for i in range(len(seera.data)):
-        name, analysis = seera.analyze_missing_values_per_project(i)
-        if analysis['ok'] == analysis['total']: complete += 1
-        print(name, analysis)
-    print('Number of projects with all the attributes after cleaning:', complete)
+    seera.add_column('Actual zeros', 'General Information', [0] * len(seera.data)) # Works
+    print('\n' + '=' * 25 + '\n=> After adding output <=\n' + '=' * 25 + '\n')
+    # analyze_significance(seera)
+    # analize_attributes(seera)
+    # analize_projects(seera)
